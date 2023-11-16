c81d7347c120f524dedd08166635171ec617beb5
MarceloMederi <marcelomederi11@gmail.com>
2023-11-16 19:19:01 -0300

    alterado o comando para entrar no repositorio

--- assets/calDin.sh
+++ assets/calDin.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/MarceloMederi/calendarioDigital
 
 # Entrar no diretório clonado
-cd cadastro-usuario
+cd calendarioDigital/assets
 
 # Construir a imagem Docker e executar o contêiner
 docker build -t app . && docker run -d -p 3000:3000 app
