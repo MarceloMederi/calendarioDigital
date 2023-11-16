e9580f3136a11db8e63c47ea31591649d152f4e0
MarceloMederi <marcelomederi11@gmail.com>
2023-11-16 20:01:40 -0300

    Alterado o dockerfile e claDin

--- assets/calDin.sh
+++ assets/calDin.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Clonar o repositório do GitHub
-git clone https://github.com/MarceloMederi/calendarioDigital
-
-# Entrar no diretório clonado
-cd calendarioDigital/assets
-
-# Construir a imagem Docker
+# Substitua 'sua-imagem' pelo nome desejado para a imagem Docker
 docker build -t app .
 
-# Executar o contêiner
-docker run -d -p 7070:7070 app
+# Execute o contêiner, mapeando a porta desejada
+docker run -p 8080:80 -d app
