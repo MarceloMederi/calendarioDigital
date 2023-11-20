453b3a35c533481cac5d161d5f5f7bb1b4be5fab
Marcelo Medeiros dos Santos <marcelo.msantos@sabin.com.br>
2023-11-20 15:20:42 -0300

    Alteração no .sh

--- calDin.sh
+++ calDin.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Substitua 'sua-imagem' pelo nome desejado para a imagem Docker
-docker build -t app .
+# Cria o diretório Nginx
+mkdir C:\Users\marcelo.msantos\nginx
 
-# Execute o contêiner, mapeando a porta desejada
-docker run -p 8080:80 -d app
+# Copia os arquivos HTML para o diretório Nginx
+cp -r . C:\Users\marcelo.msantos\nginx
+
+# Monta e executa o container do projeto
+docker build -t app . && docker run -p 8080:80 -d app
