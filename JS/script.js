010c30474800ebd610f9b20a1f0149639a90d7ed
Marcelo Medeiros dos Santos <marcelo.msantos@sabin.com.br>
2023-11-22 09:06:21 -0300

    Concluido a implementação do campo cidade

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-// Seleção de elementos HTML
 const cidadeInput = document.getElementById('cidadeInput');
 const dia = document.getElementById('dia');
 const mes = document.getElementById('mes');
@@ -11,10 +10,8 @@ const segundos = document.getElementById('segundos');
 const temperaturaElement = document.getElementById('temperatura');
 const umidadeElement = document.getElementById('umidade');
 
-// Configurações para a API OpenWeatherMap
 const apiKey = 'b60be7941bdf80361cd5c66c74dc0bb7';
 
-// Função de debounce para atrasar a execução da função de busca
 const debounce = function (func, delay) {
     let timeoutId;
     return function (...args) {
@@ -25,9 +22,21 @@ const debounce = function (func, delay) {
     };
 };
 
-// Função que busca dados da previsão do tempo com base na cidade digitada
+const buscarFusoHorario = async function (cidade) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`);
+        const data = await response.json();
+        return data.timezone; // Retorna o deslocamento de tempo em segundos
+    } catch (error) {
+        console.error('Erro ao obter fuso horário:', error);
+        return 0; // Retorna 0 em caso de erro
+    }
+};
+
 const buscarPrevisaoTempo = async function (cidade) {
     try {
+        const fusoHorario = await buscarFusoHorario(cidade);
+
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric`);
         const data = await response.json();
 
@@ -35,14 +44,44 @@ const buscarPrevisaoTempo = async function (cidade) {
         const temperaturaTexto = `${data.main.temp} °C`;
         const umidadeTexto = `${data.main.humidity}%`;
 
+        const horaLocal = luxon.DateTime.utc().plus({ seconds: fusoHorario });
+        let horasAtual = horaLocal.hour;
+        let minutosAtual = horaLocal.minute;
+        let segundosAtual = horaLocal.second;
+
+        if (horasAtual < 10) horasAtual = '0' + horasAtual;
+        if (minutosAtual < 10) minutosAtual = '0' + minutosAtual;
+        if (segundosAtual < 10) segundosAtual = '0' + segundosAtual;
+
         cidadeElement.textContent = `${nomeCidade} - ${temperaturaTexto}`;
         umidadeElement.textContent = `Umidade: ${umidadeTexto}`;
+        horas.textContent = `${horasAtual}:${minutosAtual}:${segundosAtual}`;
+
+        // Chama a função para atualizar a data
+        atualizarData(horaLocal);
+
     } catch (error) {
         console.error('Erro ao obter temperatura e umidade:', error);
     }
 };
 
-// Adiciona um evento de escuta ao input da cidade com debounce de 500 milissegundos
+// Função para atualizar a data
+function atualizarData(data) {
+    const diaDoMes = data.day;
+    const mesAtual = data.month;
+    const anoAtual = data.year;
+    const presente = data;
+
+    dia.textContent = diaDoMes;
+    mes.textContent = mesAtual;
+    ano.textContent = anoAtual;
+
+    const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const diaDaSemana = diasDaSemana[presente.weekday - 1];
+
+    atual.textContent = diaDaSemana;
+}
+
 cidadeInput.addEventListener('input', debounce(function () {
     const cidadeDigitada = cidadeInput.value;
     if (cidadeDigitada.trim() !== '') {
@@ -50,35 +89,10 @@ cidadeInput.addEventListener('input', debounce(function () {
     }
 }, 500));
 
-// Função que atualiza a hora e faz chamada à API periodicamente
-const relogio = setInterval(function time() {
-    const dateToday = new Date();
-    const diaDoMes = dateToday.getDate();
-    let mesAtual = dateToday.getMonth() + 1;
-    const anoAtual = dateToday.getFullYear();
-    const presente = dateToday;
-
-    let horasAtual = dateToday.getHours();
-    let minutosAtual = dateToday.getMinutes();
-    let segundosAtual = dateToday.getSeconds();
-
-    if (mesAtual < 10) mesAtual = '0' + mesAtual;
-    if (horasAtual < 10) horasAtual = '0' + horasAtual;
-    if (minutosAtual < 10) minutosAtual = '0' + minutosAtual;
-    if (segundosAtual < 10) segundosAtual = '0' + segundosAtual;
-
-    dia.textContent = diaDoMes;
-    mes.textContent = mesAtual;
-    ano.textContent = anoAtual;
-
-    const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
-    const diaDaSemana = diasDaSemana[presente.getDay()];
-
-    atual.textContent = diaDaSemana;
-    horas.textContent = horasAtual;
-    minutos.textContent = minutosAtual;
-    segundos.textContent = segundosAtual;
-
-    // Restante do código para buscar temperatura e umidade pode ser mantido aqui
-
+const relogio = setInterval(function () {
+    // Chama a função para buscar a previsão do tempo e ajustar a hora
+    const cidadeDigitada = cidadeInput.value;
+    if (cidadeDigitada.trim() !== '') {
+        buscarPrevisaoTempo(cidadeDigitada);
+    }
 }, 1000);
